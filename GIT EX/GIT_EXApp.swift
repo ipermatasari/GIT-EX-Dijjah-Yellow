4c98ec791a7cdf88fa6e5075191c806c7c0a3bcb
Intan Permatasari <intan.permata1606@gmail.com>
2023-05-26 15:40:50 +0700

    Initial Commit

---
+++ GIT EX/GIT_EXApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIT_EXApp.swift
+//  GIT EX
+//
+//  Created by Intan Permatasari on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct GIT_EXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
